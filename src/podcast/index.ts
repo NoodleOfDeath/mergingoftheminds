4c16dc3684f0b5f9411b8246281479c433c006fa
noodleofdeath <thom@noodleofdeath.com>
2024-01-05 15:16:33 -0500

    ep2

--- src/podcast/index.ts
+++ src/podcast/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env ts-node
 
 import 'dotenv/config';
+import { execSync } from 'child_process';
 import { ArgumentParser } from 'argparse';
 import p from 'path';
 import fs from 'fs';
@@ -20,9 +21,11 @@ parser.add_argument('-i', '--indexes', { default: undefined, nargs: '*' });
 parser.add_argument('-r', '--pattern', { default: undefined, nargs: '*' });
 parser.add_argument('-v', '--voiceId', { default: 'Vb6nEky6ikh2kpFC9qz9' });
 parser.add_argument('-w', '--overwrite', { default: undefined, action: 'store_true' });
+parser.add_argument('-d', '--diff', { default: undefined, action: 'store_true' });
 parser.add_argument('--scriptsPath', { default: p.resolve('./scripts') });
 parser.add_argument('--scriptsExt', { default: 'txt' });
 parser.add_argument('-p', '--project', { default: v4() });
+
 
 const args = parser.parse_args();
 
@@ -49,6 +52,7 @@ type ElevenLabsApiOptions = {
 type TextToSpeechOptions = ElevenLabsApiOptions & ExportOptions & {
   indexes: number[];
   pattern: string | string[];
+  diff: boolean;
   overwrite: boolean;
 }
 
@@ -60,7 +64,8 @@ export async function generateAndMerge(text: string | string[], {
   speakerBoost = true,
   indexes,
   pattern,
-  overwrite = Boolean(indexes || pattern),
+  diff,
+  overwrite = Boolean(diff || indexes || pattern),
   project = v4(),
   projectPath = p.join(args.projectRoot, project),
   outputName = 'final.wav',
@@ -69,13 +74,25 @@ export async function generateAndMerge(text: string | string[], {
   if (!fs.existsSync(projectPath)) {
     fs.mkdirSync(projectPath, { recursive: true });
   }
+  
+  if (diff) {
+    const lines = await execSync(`git diff **/*.txt | grep '^+\\w'`).toString()
+      .split('\n')
+      .map((l) => l.trim()
+         .replace(/^\+/, '')
+         .substring(0, Math.min(l.length, 50))
+         .replace(/[\+\?\[\]\{\}\(\)\|\-\^]/g, ($0) => `\\${$0}`))
+      .filter(Boolean)
+      .map((l) => `^\\s*${l}`);
+    pattern = lines;
+  }
 
   const sentences = (Array.isArray(text) ? text : text.split(/[.?!]|\\Z/)).map(s => s.trim()).filter(s => s.length > 0);
   const filenames = sentences.map((sentence, i) => p.join(projectPath, `${`${i}`.padStart(3, '0')} - ${sentence.substring(0, Math.min(sentence.length, 50))}.wav`));
   const regex = new RegExp(Array.isArray(pattern) ? pattern.join('|') : pattern, 'i');
   const matches = sentences.map((text, i) => regex.test(text) ? i : null).filter((i) => i !== null);
   indexes = Array.from(new Set(matches.length > 0 ? [...matches, ...(indexes ?? [])] : (indexes || sentences.map((_, i) => i))));
-
+  
   for (const i of indexes) {
     const sentence = sentences[i];
     if (!overwrite && fs.existsSync(filenames[i])) {
